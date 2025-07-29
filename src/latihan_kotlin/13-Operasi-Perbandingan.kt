1200520f3db2ac3b6f134ed30564da37871578f7
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2025-07-29 15:54:17 +0700

    section 1 : 13. Operasi Perbandingan

---
+++ src/latihan_kotlin/13-Operasi-Perbandingan.kt
@@ -0,0 +1,19 @@
+package latihan_kotlin
+
+fun main() {
+    val a = 100
+    val b = 200
+
+    val result: Boolean = a < b
+    println(result)
+
+    println(100 >= 100)
+    println(100 <= 100)
+
+    println("a" < "b") // hasilnya true karena angka lebih dulu secara urutan
+
+    println("a" == "a")
+
+    println("a" != "a")
+
+}
