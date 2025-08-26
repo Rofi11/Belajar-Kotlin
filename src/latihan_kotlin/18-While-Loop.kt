3b63c114c6010da9320eabd360071a87513e8754
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2025-08-26 20:09:06 +0700

    section 1 : 18. While loop di kotlin

---
+++ src/latihan_kotlin/18-While-Loop.kt
@@ -0,0 +1,14 @@
+package latihan_kotlin
+
+fun main() {
+    var i = 0
+
+    while (i < 10){
+
+        println("while loop $i")
+//        increment utk nanti bisa menghentikan perulangan
+        i++
+    }
+
+    println("Selesai perulangan")
+}
